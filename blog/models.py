fe49fab774fbb92a646470391919cc9139d541e3
olutobi1996 <olutobi1996@icloud.com>
2025-09-30 19:07:05 +0100

    blog model fix

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
 
 User = get_user_model()
+
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to='blog_images/', blank=True, null=True) 
+    image = CloudinaryField('image', blank=True, null=True)  # <-- change here
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=True)
