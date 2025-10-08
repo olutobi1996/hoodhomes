2b0cc0d841b25e0d36661400bc2a911a979e0bb6
olutobi1996 <olutobi1996@icloud.com>
2025-10-08 21:00:22 +0100

    email api set up

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_SMTP_HOST", "smtp.sendgrid.net")
 EMAIL_PORT = int(os.environ.get("EMAIL_SMTP_PORT", 587))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") in ("True", "true", "1")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")       # should be "apikey"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")  # usually 'apikey' for SendGrid
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # your SendGrid API key
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "office@hoodhomes.co.uk")
 
 
